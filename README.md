6a141521ba1c98cc61ea48969da00146fa200efa
mon33k <moniquemojica@pursuit.org>
2021-03-11 21:43:42 -0500

    fixed card flipping issue

---
+++ README.md
@@ -0,0 +1,6 @@
+-- Memory Card Game
+
+
+
+
+This wont work in IE need to find a work around to make it compatible
--- memorygame.js
+++ memorygame.js
@@ -17,17 +17,38 @@ const symbols = {
 // Hardcode the values for the cards for now 
 
 // Find way to only display the back and when you click one only two cards open at a time
+// add interval for how long two is displayed at a time and flips when it is not a match
 
 // iterate through each card div and add another div element that will be visible upon window load
 // maybe we can hev the back cards render first and then add innerHTML of the symbol for each card
+let timesClicked = 0
 
 function symbolsFunc () {
-    
-    card.forEach((e) => {
+    let temp = []
+    card.forEach((e, i) => {
         e.addEventListener( 'click', function() {
-            e.classList.toggle('is-flipped');
+            timesClicked++
+            // console.log(e.classList.value.includes('is-flipped'))
+          //  let prev = e[i].querySelector('.frontCard').innerHTML
+            console.log('e', e)
+            temp.push(e.querySelector('.frontCard').innerHTML)
+            e.classList.toggle('is-flipped')
+            if(timesClicked > 1) {
+               console.log(temp.length)
+                if(temp[0] === temp[1]) {
+                    e.classList.add('is-flipped');
+                    temp = 0
+                } else {
+                    e.classList.remove('is-flipped');
+                    console.log('not a match')
+                }
+                // console.log(e.querySelector('.frontCard'))
+
+            }
+
         })
     })
 }
 
 symbolsFunc()
+console.log(timesClicked)
